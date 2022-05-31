00d8483c8c34dbf8f141549a64d1c79a341230f9
Hannes <50320115+hienous@users.noreply.github.com>
2022-05-31 09:52:48 +0300

    added blockingMode param to getCookieScript

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,15 +7,24 @@
 /**
  * Returns Cookiebot script string with data-cbid based on value parameter.
  * @param {string} [value=''] Cookiebot data-cbid value.
+ * @param {boolean} [blockingMode=true] Cookiebot blocking mode,
+ * default to automatically block cookies and false when cookies are manually blocked.
  * @returns {CookieBotScript} string containing the script.
  */
-function getCookieScript(value= '') {
+function getCookieScript(value= '', blockingMode = true) {
     if (typeof value !== 'string') {
         throw new Error('Cookiebot cbid-data value is not a string!')
     }
+    // Contains values which change according to blockingMode.
+    const blockAttributes = {
+        // 'data-blockingmode="auto"' if blockingMode is true, otherwise ''.
+        block: blockingMode ? `data-blockingmode="auto"` : '',
+        // 'async' if blockingMode is false, otherwise ''.
+        async: !blockingMode ? ' async' : '',
+    };
     return `
         <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" 
-        data-cbid=${value} data-blockingmode="auto" type="text/javascript"></script>
+        data-cbid=${value} ${blockAttributes.block} type="text/javascript"${blockAttributes.async}></script>
     `;
 }
 
